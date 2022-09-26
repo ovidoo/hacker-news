945b7efabd4a65896512cc540b9f9fda9f77bbfc
ovidoo <filos_ovidiu@yahoo.com>
2022-09-27 00:08:53 +0100

    added rxjs

--- hn-feed/src/features/news/newsApi.ts
+++ hn-feed/src/features/news/newsApi.ts
@@ -1,6 +1,7 @@
 import {ArticleComment, NewsArticle} from "./newsSlice";
 import {getStoredState} from "../../app/utils";
 import {keys, values} from "lodash";
+import {concatMap, finalize, from, map, mergeMap, observable, Observable, of, switchMap} from "rxjs";
 
 export interface FetchOptions {
     latestStories: number[];
@@ -24,14 +25,26 @@ export async function fetchNews(options: FetchOptions): Promise<NewsArticle[]> {
     const savedStories = values(getStoredState(true)?.articlesMap);
     const savedStoriesKeys: number[] = savedStories.map(d => d.id);
     const data = latestStories.slice(startFrom, startFrom + storiesPerPage).filter(d => !savedStoriesKeys.includes(d));
-    const allNews = await Promise.all(data.map(async (d) => await (await fetchStory(d)).json()))
+    const allNews = await Promise.all(data.map(async (d) => await fetchStory(d)))
     console.log('fetchNews | data=', data);
     return [...savedStories, ...allNews];
 }
 
 export async function fetchStory(id: number): Promise<any> {
-    return fetch(storyUrl(id));
+    const res = await fetch(storyUrl(id));
+    return res.json();
 }
 
-
-
+export function fetchNewsObs(options: FetchOptions): Observable<any> {
+    let { latestStories, storiesPerPage = 12, startFrom = 0 } = options;
+    const savedStories = values(getStoredState(true)?.articlesMap);
+    const savedStoriesKeys: number[] = savedStories.map(d => d.id);
+    const data = latestStories.slice(startFrom, startFrom + storiesPerPage).filter(d => !savedStoriesKeys.includes(d));
+    const sub = from(data)
+        .pipe(
+            mergeMap(async (d) => await fetchStory(d))
+        )
+        // .subscribe((val) => console.log('subscribtion >>> ', allNews = [...allNews, val]));
+    console.log('fetchNews | data=', data);
+    return sub;
+}
